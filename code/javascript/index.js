ed6d642d5b954a47b72ca6fd5876ef9674ae3c70
greenjm <greenjm@rose-hulman.edu>
2015-04-22 19:40:35 -0400

    Minor bugs

--- code/javascript/index.js
+++ code/javascript/index.js
@@ -1,10 +1,9 @@
 var showinfo = function(id) {
 	console.log("here", id);
 	$("#" + id).show();
-	$("#" + id).animate({height: "30%"}, 500);
+	$("#" + id).animate({height: "30%"}, 750);
 }
 var closeinfo = function(id) {
-	$("#" + id).animate({height: "5%"}, 500, function() {
 		$("#" + id).hide();
 	});
 	//$("#" + id).hide();
