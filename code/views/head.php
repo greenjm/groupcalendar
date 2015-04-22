5e86a363246b789d321c0e8a43ff7ed1c860cbac
greenjm <greenjm@rose-hulman.edu>
2015-04-21 23:36:14 -0400

    Finished rough idea for front page

---
+++ code/views/head.php
@@ -0,0 +1,11 @@
+<head>
+	<!--js libraries-->
+	<script src="../libraries/jquery-2.1.3.min.js"></script>
+	<script src="../libraries/bootstrap.min.js"></script>
+	<script src="../libraries/npm.js"></script>
+	<!--css stylesheets-->
+	<link rel="stylesheet" type="text/css" href="../libraries/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../libraries/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="../styles/main.css">
+
+	<link rel="icon" type="image/png" href="../../resources/images/cal_favicon.jpg">
---
+++ code/views/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<?php
+	include "head.php";
+?>
+</head>
+<body>
+	<?php include "nav.html"; ?>
+  <div id="gc_about_cont">
+  <div class="jumbotron" id="gc_about_img">
+  <div id="gc_about_img_text">
+  <h1>What is Group Calendar?</h1>
+  <p>Group Calendar is an awesome system that allows you to manage your own calendar and coordinate with others for group meetings.</p>
+  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a></p>
+</div>
+  <div class="jumbotron container lm_text" id="gc_about">
+    <p>Lorem ipsum dolor sit amet, dico dicta minimum vix cu, id cibo putant duo. Id est dicat probatus deseruisse, ne eam omnes cetero. Usu cibo errem labitur no, dicunt integre et cum. Pro ut dignissim incorrupte. Elit partem doming ne sea, option offendit ei mea. Eu dicam oratio convenire eos, dolore audiam et mea. Volutpat accusamus quaerendum no eum.
+
+Postulant tincidunt usu et, sit habeo vulputate intellegam at, eos graeci admodum molestie ea. Ad iisque iuvaret his. Nam dolorum percipitur ne. Sed ut erat ipsum laboramus, ut autem ipsum persecuti nec. Nusquam vituperata repudiandae eu qui, eu tale dolore dissentias vis, mel ne omnes moderatius.</p>
+<p><a class="btn btn-primary btn-ls" href="#" role="button">Close</a></p>
+  </div>
+</div>
+</div>
+<div class="filler"></div>
+<div id="gc_hiw_cont">
+<div class="jumbotron" id="gc_hiw_img">
+<div id="gc_hiw_img_text">
+  <h1>How Does it Work?</h1>
+  <p>Group Calendar works by taking in all of your group's information and finding times when all of you are free to work on your deadlines.</p>
+  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a></p>
+</div>
+<div class="jumbotron container lm_text">
+<p>Lorem ipsum dolor sit amet, dico dicta minimum vix cu, id cibo putant duo. Id est dicat probatus deseruisse, ne eam omnes cetero. Usu cibo errem labitur no, dicunt integre et cum. Pro ut dignissim incorrupte. Elit partem doming ne sea, option offendit ei mea. Eu dicam oratio convenire eos, dolore audiam et mea. Volutpat accusamus quaerendum no eum.
+
+  Postulant tincidunt usu et, sit habeo vulputate intellegam at, eos graeci admodum molestie ea. Ad iisque iuvaret his. Nam dolorum percipitur ne. Sed ut erat ipsum laboramus, ut autem ipsum persecuti nec. Nusquam vituperata repudiandae eu qui, eu tale dolore dissentias vis, mel ne omnes moderatius.</p>
+<p><a class="btn btn-primary btn-ls" href="#" role="button">Close</a></p>
+</div>
+</div>
+</div>
+</body>
